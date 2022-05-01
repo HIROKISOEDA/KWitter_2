03014fd0abcca6c860cdfb4cbfbe5005953dc71c
HIROKISOEDA <90941391+HIROKISOEDA@users.noreply.github.com>
2022-05-01 19:52:41 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCp2udhz-XR3h_BLjfuVRrLLm1gvhFg_U",
+  authDomain: "kwitter-room-41a5c.firebaseapp.com",
+  databaseURL: "https://kwitter-room-41a5c-default-rtdb.firebaseio.com",
+  projectId: "kwitter-room-41a5c",
+  storageBucket: "kwitter-room-41a5c.appspot.com",
+  messagingSenderId: "864465158722",
+  appId: "1:864465158722:web:7f7260b18069eb341323db"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
